01bdf1791f3b440895df3f4cd90a608df01ddeff
imluque <luque.eric44@gmail.com>
2026-01-07 10:36:12 +0100

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -47,11 +47,11 @@ export const defaultContentPageLayout: PageLayout = {
     zoom: true, // whether to allow zooming in and out
     depth: 1, // how many hops of notes to display
     scale: 1.1, // default view scale
-    repelForce: 0.5, // how much nodes should repel each other
-    centerForce: 0.3, // how much force to use when trying to center the nodes
+    repelForce: 0.8, // how much nodes should repel each other
+    centerForce: 0.2, // how much force to use when trying to center the nodes
     linkDistance: 300, // how long should the links be by default?
-    fontSize: 0.6, // what size should the node labels be?
-    opacityScale: 1, // how quickly do we fade out the labels when zooming out?
+    fontSize: 0.5, // what size should the node labels be?
+    opacityScale: 1.2, // how quickly do we fade out the labels when zooming out?
     removeTags: [], // what tags to remove from the graph
     showTags: true, // whether to show tags in the graph
     enableRadial: false, // whether to constrain the graph, similar to Obsidian
@@ -59,16 +59,16 @@ export const defaultContentPageLayout: PageLayout = {
   globalGraph: {
     drag: true,
     zoom: true,
-    depth: -1,
+    depth: 2,
     scale: 0.9,
-    repelForce: 0.5,
-    centerForce: 0.3,
-    linkDistance: 300,
-    fontSize: 0.6,
-    opacityScale: 1,
+    repelForce: 1,
+    centerForce: 0.15,
+    linkDistance: 500,
+    fontSize: 0.45,
+    opacityScale: 1.4,
     removeTags: [], // what tags to remove from the graph
-    showTags: true, // whether to show tags in the graph
-    enableRadial: true, // whether to constrain the graph, similar to Obsidian
+    showTags: false, // whether to show tags in the graph
+    enableRadial: false, // whether to constrain the graph, similar to Obsidian
   },
 }),
     Component.DesktopOnly(Component.TableOfContents()),
