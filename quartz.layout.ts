2d5d4a0b99417089fdd84a5163293db22a5a2a41
imluque <luque.eric44@gmail.com>
2026-01-07 10:26:59 +0100

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -49,7 +49,7 @@ export const defaultContentPageLayout: PageLayout = {
     scale: 1.1, // default view scale
     repelForce: 0.5, // how much nodes should repel each other
     centerForce: 0.3, // how much force to use when trying to center the nodes
-    linkDistance: 30, // how long should the links be by default?
+    linkDistance: 300, // how long should the links be by default?
     fontSize: 0.6, // what size should the node labels be?
     opacityScale: 1, // how quickly do we fade out the labels when zooming out?
     removeTags: [], // what tags to remove from the graph
@@ -63,7 +63,7 @@ export const defaultContentPageLayout: PageLayout = {
     scale: 0.9,
     repelForce: 0.5,
     centerForce: 0.3,
-    linkDistance: 3,
+    linkDistance: 300,
     fontSize: 0.6,
     opacityScale: 1,
     removeTags: [], // what tags to remove from the graph
